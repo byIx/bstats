b9a35b438342a42d3c3a9622143467578d02dd5a
byIx <40878600+byIx@users.noreply.github.com>
2022-01-16 02:54:07 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.1.0", "longversion":11000, "released":1642269600, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2492-customize-admin-login-page\/" }
+{ "version":"1.1.0", "longversion":11000, "released":1642269600, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2499-better-statistics\/" }
